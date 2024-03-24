3cd583ce4a24abde10c4e0691fc5d73545cac4cc
Shivam-Amin <106419448+Shivam-Amin@users.noreply.github.com>
2024-03-24 16:21:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 export const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Allow credentials (cookies)
   optionsSuccessStatus: 204, // Respond with a 204 status for preflight requests
